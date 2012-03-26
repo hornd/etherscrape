4c21bc9a42ec1ba348c52ee418948606c84ab54f
Douglas Horn <douglas.j.horn@gmail.com>
2012-03-26 14:38:30 +0000

    Begin grid print

--- src/pretty_print.c
+++ src/pretty_print.c
@@ -6,13 +6,22 @@
 /*debug*/
 #include "layer2.h"
 
-static char* 
-get_asterisks();
+static void
+print_asterisks();
+
+static void
+print_header();
+
+static char*
+get_string_format();
 
 extern void 
 display_grid()
 {
     struct pack_cap *hd;
+    
+    print_header();
+
     FOREACH_PL(hd)
     {
         print_out(hd);
@@ -22,16 +31,52 @@ display_grid()
 extern void
 print_out(struct pack_cap *pack)
 {
-    printf("%s\n", get_asterisks());
-    printf("Packet Length: %u\n", pack->packet_len);
-    printf("%s\n", get_asterisks());
 
-    debug_print_l2(pack); 
+    layer2_header *eth_header;
+    uint8_t *ptr, i;
+    eth_header = (layer2_header *)(pack->packet);
+    ptr = eth_header->dest_addr;
+
+    printf("%-5d | %-8s |", pack->packet_id, "2ms");
+
+    /* Debug only... needs to be IP */
+    for(i=0; i<HARDWARE_ADDRESS_LEN; i++)
+    {
+        printf("%s%02x", i==0?" ":":", *ptr++);
+    }
+
+    printf("\n");
 }
 
+#define ID_FORMAT "%-5s"
+#define TIME_FORMAT "%-8s"
+#define SOURCE_FORMAT "%-18s"
+#define DEST_FORMAT SOURCE_FORMAT
+#define TYPE_FORMAT "%-10s"
+#define LENGTH_FORMAT "%-5s"
+
+static void
+print_header()
+{
+    #define LINE_LENGTH 72
+
+    uint8_t i = LINE_LENGTH;
+
+    printf(get_string_format(), "ID", "Time Rx", "Source IP", "Dest IP", "Type", "Length");
+
+    while(i--) printf("-");
+    printf("\n");
+    
+}
 
 static char*
-get_asterisks()
+get_string_format()
 {
-    return "******************************";
+    return ID_FORMAT " | " TIME_FORMAT " | " SOURCE_FORMAT " | " DEST_FORMAT " | " TYPE_FORMAT " | " LENGTH_FORMAT " |\n";
+}
+
+static void
+print_asterisks()
+{
+    printf("******************************\n");
 }
