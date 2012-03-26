23f23c48fa347bbaae212b288914214d5b0cc329
Douglas Horn <douglas.j.horn@gmail.com>
2012-03-26 15:18:27 +0000

    More print stuff

--- src/pretty_print.c
+++ src/pretty_print.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdint.h>
+#include <string.h>
 #include "pack_list.h"
 #include "pretty_print.h"
 
@@ -20,7 +21,7 @@ display_grid()
 {
     struct pack_cap *hd;
     
-    print_header();
+    print_header(); 
 
     FOREACH_PL(hd)
     {
@@ -33,33 +34,44 @@ print_out(struct pack_cap *pack)
 {
 
     layer2_header *eth_header;
-    uint8_t *ptr, i;
-    eth_header = (layer2_header *)(pack->packet);
-    ptr = eth_header->dest_addr;
+    uint8_t *ptr, i, loc_ptr = 0;
+    char buffer[100];
 
-    printf("%-5d | %-8s |", pack->packet_id, "2ms");
+    eth_header = (layer2_header *)(pack->packet);
+    ptr = eth_header->source_addr;
+
+    printf("%-5d | %-8s |", pack->packet_id, "2ms"); 
 
     /* Debug only... needs to be IP */
     for(i=0; i<HARDWARE_ADDRESS_LEN; i++)
     {
-        printf("%s%02x", i==0?" ":":", *ptr++);
+        loc_ptr += sprintf(buffer+loc_ptr, "%s%02x", i==0?" ":":", *ptr++);
     }
+    buffer[loc_ptr] = '\0';
+
+    printf("%-18s  |", buffer);
+
+    memset(buffer, 0, loc_ptr);
+    loc_ptr = 0;
+    ptr = eth_header->dest_addr;
+
+    for(i=0; i<HARDWARE_ADDRESS_LEN; i++)
+    {
+        loc_ptr += sprintf(buffer+loc_ptr, "%s%02x", i==0?" ":":", *ptr++);
+    }
+
+    printf("%-18s  | ", buffer);
+    printf("%-8s | ", "0x0800");
+    printf("%-5d ", pack->packet_len);
 
     printf("\n");
 }
 
-#define ID_FORMAT "%-5s"
-#define TIME_FORMAT "%-8s"
-#define SOURCE_FORMAT "%-18s"
-#define DEST_FORMAT SOURCE_FORMAT
-#define TYPE_FORMAT "%-10s"
-#define LENGTH_FORMAT "%-5s"
+#define LINE_LENGTH 72
 
 static void
 print_header()
 {
-    #define LINE_LENGTH 72
-
     uint8_t i = LINE_LENGTH;
 
     printf(get_string_format(), "ID", "Time Rx", "Source IP", "Dest IP", "Type", "Length");
@@ -68,6 +80,13 @@ print_header()
     printf("\n");
     
 }
+
+#define ID_FORMAT "%-5s"
+#define TIME_FORMAT "%-8s"
+#define SOURCE_FORMAT "%-18s"
+#define DEST_FORMAT SOURCE_FORMAT
+#define TYPE_FORMAT "%-8s"
+#define LENGTH_FORMAT "%-5s"
 
 static char*
 get_string_format()
